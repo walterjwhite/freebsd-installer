31d269e7f1998ccd8232cec3dc16ad44eb451368
Walter White <WalterWhite@walterjwhite.com>
2021-02-06 08:18:28 -0500

    merged

--- lib/bsdinstall/cli.sh
+++ lib/bsdinstall/cli.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 _require "$NET" "NET is required (ie. msk0)" 1
-ifconfig $NET 2>&1 > /dev/null
-if [ "$?" -gt "0" ]
-then
+ifconfig $NET 2>&1 >/dev/null
+if [ "$?" -gt "0" ]; then
 	exitWithError "$NET does not appear to exist" 2
 fi
 
 _require "$DEV" "DEV is required (ie. ada0)" 3
 _require "$DEV_NAME" "DEV_NAME is required (ie. 250.5 - NOTE: device will automatically be prefixed with 'z_')" 4
 _INCORRECT_PREFIX=$(echo $DEV_NAME | grep -c ^z_)
-if [ "$_INCORRECT_PREFIX" -gt "0" ]
-then
+if [ "$_INCORRECT_PREFIX" -gt "0" ]; then
 	DEV_NAME=$(echo $DEV_NAME | sed -e "s/^z_//")
 fi
 
@@ -23,7 +21,6 @@ _require "$BRANCH" "BRANCH is required (ie. workstation)" 7
 GIT_HOST=$(echo $GIT | sed -e "s/\:.*$//")
 export GIT_HOST
 
-if [ -z "$_SSH_KEYTYPE" ]
-then
+if [ -z "$_SSH_KEYTYPE" ]; then
 	_SSH_KEYTYPE=ecdsa
 fi
--- lib/setup/libraries/cli.sh
+++ lib/setup/libraries/cli.sh
@@ -1,54 +1,50 @@
 #!/bin/sh
 
 _cleanup() {
-  if [ -n "$_SYSTEM_REPOSITORY_PATH" ]
-  then
-    rm -rf $_SYSTEM_REPOSITORY_PATH
-  fi
+	if [ -n "$_SYSTEM_REPOSITORY_PATH" ]; then
+		rm -rf $_SYSTEM_REPOSITORY_PATH
+	fi
 }
 
 trap _cleanup INT
 
-for _ARG in $@
-do
-  case $_ARG in
-    # NOT supported presently
-    # -u)
-    #   _is_default=$(beadm list | grep N | grep -c ^default)
-    #   if [ "$_is_default" -eq "0" ]
-    #   then
-    #     _ beadm activate default
-    #     _ reboot
-    #   else
-    #     echo "default BE is already activated, continuiing"
-    #   fi
+for _ARG in $@; do
+	case $_ARG in
+	# NOT supported presently
+	# -u)
+	#   _is_default=$(beadm list | grep N | grep -c ^default)
+	#   if [ "$_is_default" -eq "0" ]
+	#   then
+	#     _ beadm activate default
+	#     _ reboot
+	#   else
+	#     echo "default BE is already activated, continuiing"
+	#   fi
 
-    #   ;;
-    -b=*)
-      _BRANCH="${_ARG#*=}"
-      ;;
-    *)
-      _SYSTEM_REPOSITORY_PATH=$(mktemp -d)
+	#   ;;
+	-b=*)
+		_BRANCH="${_ARG#*=}"
+		;;
+	*)
+		_SYSTEM_REPOSITORY_PATH=$(mktemp -d)
 
-      # bypass the host checking (to avoid timeouts)
-      #ssh -o "StrictHostKeyChecking no" $(echo $_ARG | sed -e "s/\:.*//")
-      #exit
-      mkdir -p ~/.ssh
-      echo "StrictHostKeyChecking no" >> ~/.ssh/config
+		# bypass the host checking (to avoid timeouts)
+		#ssh -o "StrictHostKeyChecking no" $(echo $_ARG | sed -e "s/\:.*//")
+		#exit
+		mkdir -p ~/.ssh
+		echo "StrictHostKeyChecking no" >>~/.ssh/config
 
-      git clone $_ARG $_SYSTEM_REPOSITORY_PATH
+		git clone $_ARG $_SYSTEM_REPOSITORY_PATH
 
-      cd $_SYSTEM_REPOSITORY_PATH
-      ;;
-  esac
+		cd $_SYSTEM_REPOSITORY_PATH
+		;;
+	esac
 done
 
-if [ -z "$_SYSTEM_REPOSITORY_PATH" ]
-then
-  exitWithError "no git repository URL was specified" 1
+if [ -z "$_SYSTEM_REPOSITORY_PATH" ]; then
+	exitWithError "no git repository URL was specified" 1
 fi
 
-if [ -n "$_BRANCH" ]
-then
-  git checkout $_BRANCH
+if [ -n "$_BRANCH" ]; then
+	git checkout $_BRANCH
 fi
